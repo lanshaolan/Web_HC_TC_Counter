efd00f3d0c93a1f924879f26dc8ad03e16348914
KID421 <suchild7@gmail.com>
2020-06-28 15:26:17 +0800

    頁面準備完成時取得元素位移資料

---
+++ counter.js
@@ -0,0 +1,8 @@
+// 當頁面 準備完成時
+$(document).ready(function () {
+    
+    // var 變數 = 資料
+    var offset = $("#test").offset();       // 取得元素位移資料
+    var top = offset.top;                   // 上方
+    var left = offset.left;                 // 左方
+});
